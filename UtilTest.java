c1b32032859982b116fd50bd62b55a235a4a3ada
Bryan Glazer <bglazer@uark.edu>
2013-01-10 20:39:14 -0600

    Added a unit test file for the Utility class

---
+++ UtilTest.java
@@ -0,0 +1,26 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+ 
+
+public class UtilTest{
+    @Test
+    public void testSqDistArrayList()
+    {}
+
+    @Test
+    public void testSqDistArray()
+    {}
+
+    @Test
+    public void testMapArray()
+    {}
+
+    @Test
+    public void testMapSingle()
+    {}
+
+    @Test
+    public void testMapUnitSquare()
+    {}
+}

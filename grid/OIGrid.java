485742d28f1cfa5659740741e1bc559f659f9c7f
Bryan Glazer <bglazer@uark.edu>
2013-01-13 22:08:59 -0600

    Removed flood fill algorithm for finding near points. Beginning to rework with a simpler overhaul algorithm.

--- grid/OIGrid.java
+++ grid/OIGrid.java
@@ -1,6 +1,6 @@
 package naybur.grid;
 
-import naybur.Utility;
+import static naybur.Utility.*;
 import java.util.Comparator;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -15,24 +15,22 @@ class OIGrid
     private int len;
 
     private double[][] point_list;
-    private double[] range[];
+    private double[][] range;
 
     public OIGrid(double[][] points, double[][] range)
     {
+        delta = 1/Math.sqrt(point.length);
+
         grid = new LinkedList[(int)(1/delta)][(int)(1/delta)];
-        overhaul(points);
 
         this.range = range;
-        double[] end_range = { 0,1 }; 
-        points = Utility.map(points, range, end_range, 2);
+        this.point_list = Utility.map(points, range);
+
+        overhaul(point_list);
     }
 
     public void overhaul(double[][] points)
     {
-        this.delta = delta;
-
-        point_list = points;
-
         for(int i = 0; i < point_list.length; i++)
         {
             int cell_index_x = (int)Math.floor(point_list[i][0]/delta);
@@ -49,9 +47,7 @@ class OIGrid
 
     public double[][] findNearest(double[] search_point, int k)
     {
-        double[] end_range  = {0,1};
-        
-        double[] searchp_mapped = Utility.map(search_point, range, end_range, 2); 
+        double[] searchp_mapped = map(search_point, range); 
 
         int[] search_cell = { (int)Math.floor(searchp_mapped[0]/delta), (int)Math.floor(searchp_mapped[1]/delta) } ;
 
@@ -75,7 +71,6 @@ class OIGrid
     {
         public int len;
         public int[] cent;
-        public ArrayList points;
 
         public Rect(int len, int[] cent)
         {
@@ -83,91 +78,32 @@ class OIGrid
             this.cent = cent;
         }
 
-        public ArrayList findPoints(int k)
+        public ArrayList<Double> findPoints(int k)
         {
-            ArrayList points = new ArrayList();
+            points_found = 0;
 
-            boolean[][] visited = new boolean[grid.length][grid[0].length];
-            LinkedList queue = new LinkedList(), next_queue = new LinkedList();
+            LinkedList<Double> points = new LinkedList<Double>();
+            int width = grid[0].length;
+            int height = grid.length;
 
-            int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-            next_queue.push(cent);
-            visited[cent[0]][cent[1]] = true;
-
-            while(points.size() < k)
+            while(points_found < k)
             {
-                queue = next_queue;
-                next_queue.clear();
+                int start_x = cent[0] - len < 0 ? 0 : cent[0] - len;
+                int end_x = cent[0] + len > width ? 0 : cent[0] + len;
 
-                while(!queue.isEmpty())
+                int start_y = cent[1] - len < 0 ? 0 : cent[1] - len;
+                int end_y = cent[1] + len > height ? 0 : cent[1] + len;
+
+                for(int i = start_x; i < end_x; i++)
                 {
-                    int[] cell = (int[])queue.pop();
-                    points.addAll(grid[cell[0]][cell[1]]);
-
-                    //North
-                    int[] temp_cell = { cell[0], cell[1] + 1};
-                    if(temp_cell[1] < grid.length && !visited[temp_cell[0]][temp_cell[1]])
+                    for(int j = start_y; j < end_y; j++)
                     {
-                        if(temp_cell[1] > cent[1] + len)
-                            next_queue.push(temp_cell);
-                    }
-                    else if(!visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        queue.push(temp_cell);
-                        points.addAll(grid[temp_cell[0]][temp_cell[1]]);
-                    }
-
-                    //South
-                    temp_cell[0] = cell[0];
-                    temp_cell[1] = cell[1];
-
-                    if(temp_cell[1] >= 0 && !visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        if(temp_cell[1] < cent[1] - len)
-                            next_queue.push(grid[temp_cell[0]][temp_cell[1]]);
-                    }
-                    else if(!visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        queue.push(temp_cell);
-                        points.addAll(grid[temp_cell[0]][temp_cell[1]]);
-                    }
-
-                    //East
-                    temp_cell[0] = cell[0] + 1;
-                    temp_cell[1] = cell[1];
-
-                    if(temp_cell[0] < grid.length && !visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        if(temp_cell[0] > cent[0] + len)
-                            next_queue.push(grid[temp_cell[0]][temp_cell[1]]);
-                    } 
-                    else if(!visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        queue.push(temp_cell);
-                        points.addAll(grid[temp_cell[0]][temp_cell[1]]);
-                    }
-
-                    //West
-                    temp_cell[0] = cell[0] - 1;
-                    temp_cell[1] = cell[1];
-
-                    if(temp_cell[0] >= 0 && !visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        if(temp_cell[0] < cent[0] - len)
-                            next_queue.push(grid[temp_cell[0]][temp_cell[1]]);
-                    }
-                    else if(!visited[temp_cell[0]][temp_cell[1]])
-                    {
-                        queue.push(temp_cell);
-                        points.addAll(grid[temp_cell[0]][temp_cell[1]]);
+                        points.addAll(grid[i][j]);
                     }
                 }
+
+                len++;
             }
-
-            len++;
-
-            return points;
         }
     }
 

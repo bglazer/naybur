ec30789306312a57ac041a8524f45698358dd249
bryan <bryan@bryan-XPS-M1330.(none)>
2013-09-14 12:39:07 -0500

    Renamed test_point_list, added TestWatcher

--- grid/OITest.java
+++ grid/OITest.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.*;
 import java.util.*;
 import static naybur.Utility.*;
 import naybur.Point;
+import naybur.grid.GridTestWatcher;
  
 public class OITest{
  
-    private ArrayList<Point> point_list, test_point_list; 
+    private ArrayList<Point> point_list, search_point_list; 
     private OIGrid grid;
     private int scale_x = 10, scale_y = 10; 
     private int num_points = 10;
@@ -19,6 +20,9 @@ public class OITest{
     public void setUp()
     {
     }
+
+    @Rule
+    public GridTestWatcher watcher = new GridTestWatcher();
 
     public ArrayList<Point> createTestList(int num_point_list, boolean integers, boolean negative)
     {
@@ -48,18 +52,20 @@ public class OITest{
         return list;
     } 
 
-    public void compareResults(ArrayList<Point> point_list, ArrayList<Point> test_point_list)
+    public void compareResults(ArrayList<Point> point_list, ArrayList<Point> search_point_list)
     {
         System.out.println("compareResults");
+        watcher.setList(point_list);
         
-        for(Point search_point: test_point_list)
+        for(Point search_point: search_point_list)
         {
             int num_results = 1;
             
             int grid_result = grid.findNearest(search_point, num_results).get(0);
             int linear_result = linearSearch(point_list, search_point);
             
-            assertEquals(grid_result, linear_result);
+            watcher.setPoint(search_point);
+            assertEquals(linear_result, grid_result);
         }
 
     }
@@ -81,7 +87,7 @@ public class OITest{
         System.out.println("\ntestBuildGrid\n");
         ArrayList<Point> point_list = createTestList(num_points, false, false);
 
-        printPoints(point_list);
+        //printPoints(point_list);
 
         grid = new OIGrid(point_list, pos_range);
     }
@@ -89,30 +95,33 @@ public class OITest{
     @Test
     public void testSearchCustom()
     {
-        double[][] point_array = { {-6.0, -4.0}, {-8.0, -10.0}, {2.0, -2.0}, {-2.0, -10.0}, {-10.0, 8.0}, {-10.0, 6.0}, {-2.0, 0.0}, {0.0, 2.0}, {8.0, -10.0}, {-8.0, -10.0} };
+        double[][] point_array =
+        {
+{5.0, 2.0}, {1.0, 0.0}, {3.0, 6.0}, {3.0, 9.0}, {9.0, 2.0}, {9.0, 6.0}, {3.0, 4.0}, {6.0, 5.0}, {8.0, 0.0}, {7.0, 3.0}
+};
+        ArrayList<Point> point_list = new ArrayList<Point>();
 
-        for(int i = 0; i < point_list.size(); i++)
+        for(int i = 0; i < point_array.length; i++)
         {
             double x = point_array[i][0];
             double y = point_array[i][1];
 
             Point p = new Point(x,y);
-
             point_list.add(p);     
         }
 
-        OIGrid grid = new OIGrid(point_list, neg_range);
+        OIGrid grid = new OIGrid(point_list, pos_range);
 
-        Point search_point = new Point(-4.0, 6.0);
+        Point search_point = new Point(2.0,6.0);
 
-        printPoints(point_list);
+        //printPoints(point_list);
 
         int num_results = 1;
             
         int grid_result = grid.findNearest(search_point, num_results).get(0);
         int linear_result = linearSearch(point_list, search_point);
         
-        assertEquals(grid_result, linear_result);
+        assertEquals(linear_result, grid_result);
     } 
 
     @Test
@@ -120,14 +129,14 @@ public class OITest{
     {
         System.out.println("\ntestSearchRandomIntegerPos\n");
         point_list = createTestList(num_points, true, false);
-        test_point_list = createTestList(num_points, true, false);
+        search_point_list = createTestList(num_points, true, false);
 
         grid = new OIGrid(point_list, pos_range);
 
 //        printPoints(point_list);
-//        printPoints(test_point_list);
+//        printPoints(search_point_list);
 
-        compareResults(point_list, test_point_list);
+        compareResults(point_list, search_point_list);
     }
 
     @Test 
@@ -135,14 +144,14 @@ public class OITest{
     {
         System.out.println("\ntestSearchRandomDoublePos\n");
         point_list = createTestList(num_points, false, false); 
-        test_point_list = createTestList(num_points, false, false);
+        search_point_list = createTestList(num_points, false, false);
 
         grid = new OIGrid(point_list, pos_range);
 
 //        printPoints(point_list);
-//        printPoints(test_point_list);
+//        printPoints(search_point_list);
 
-        compareResults(point_list, test_point_list);
+        compareResults(point_list, search_point_list);
     }
 
     @Test
@@ -150,14 +159,14 @@ public class OITest{
     {
         System.out.println("\ntestSearchRandomIntegerNeg\n");
         point_list = createTestList(num_points, true, true);
-        test_point_list = createTestList(num_points, true, true);
+        search_point_list = createTestList(num_points, true, true);
 
         grid = new OIGrid(point_list, neg_range);
 
 //        printPoints(point_list);
-//        printPoints(test_point_list);
+//        printPoints(search_point_list);
 
-        compareResults(point_list, test_point_list);
+        compareResults(point_list, search_point_list);
     }
 
     @Test
@@ -165,14 +174,13 @@ public class OITest{
     {
         System.out.println("\ntestSearchRandomDoubleNeg\n");
         point_list = createTestList(num_points, false, true); 
-        test_point_list = createTestList(num_points, false, true);
+        search_point_list = createTestList(num_points, false, true);
 
         grid = new OIGrid(point_list, neg_range);
 
 //        printPoints(point_list);
-//        printPoints(test_point_list);
+//        printPoints(search_point_list);
 
-        compareResults(point_list, test_point_list);
+        compareResults(point_list, search_point_list);
     }
-    
 }

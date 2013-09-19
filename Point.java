b5a62eaa34e84594546f6c5bf68d37753bee9531
bryan <bryan@bryan-XPS-M1330.(none)>
2013-09-18 23:13:07 -0500

    Removed all reference to coordinates ArrayList

--- Point.java
+++ Point.java
@@ -4,37 +4,19 @@ import java.util.ArrayList;
 
 public class Point
 {
-    public ArrayList<Double> coordinates;
+    public double x,y;
     public int dims;
 
     public Point(double x, double y)
     {
-        this.coordinates = new ArrayList<Double>();
+        this.x = x;
+        this.y = y;
 
-        coordinates.add(x);
-        coordinates.add(y);
-
-        dims = coordinates.size();
-    }
-
-    public Point(ArrayList<Double> coordinates)
-    {
-        this.coordinates = coordinates;
-        dims = coordinates.size();
-    }
-
-    public Double x()
-    {
-        return coordinates.get(0);
-    }
-
-    public Double y()
-    {
-        return coordinates.get(1);
+        dims = 2;
     }
 
     public String toString()
     {
-        return "{" + x() + ", " + y() + "}";
+        return "{" + x + ", " + y + "}";
     }
 }
